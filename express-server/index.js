51b6f7c26c4f2831e147ceccecde20dd6a263ebd
Sergiu Buruiana <sergiu.buru99@gmail.com>
2021-07-14 12:37:52 -0500

    Added web socket functionality to communicate with the service worker

--- express-server/index.js
+++ express-server/index.js
@@ -1,8 +1,31 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const ws = require('ws');
+const app = express();
 
-app.get('/', function(req,res) {
-  res.send("hello world!");
+const wss = new ws.Server({port: 3000});
+wss.on('connection', socket => {
+  console.log('connection established');
+  socket.on('message', (message) => {
+    console.log(message);
+    // socket.send('hello there');
+    // if(message === "Requesting room id") {
+    //   socket.send("75");
+    // }
+  });
 });
 
-app.listen(3000);
+// app.get('/', function(req,res) {
+//   res.send("hello world!");
+// });
+
+// app.get('/extensions', function(req,res) {
+//   res.send("message to extension");
+//   console.log("get request at /extensions");
+// });
+
+// const server = app.listen(3000);
+// server.on('upgrade', (req, soket, head) => {
+//   wss.handleUpgrade(req, socket, head, socket => {
+//     wss.emit('connection', socket, req);
+//   });
+// });
