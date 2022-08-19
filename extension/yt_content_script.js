346874a076a9a6ec4eddb75563a4d2785b1763d8
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-08-19 16:58:50 -0500

    Added functionality for waiting until youtube ads go away so the yt script can get access to the youtube video element

--- extension/yt_content_script.js
+++ extension/yt_content_script.js
@@ -1,12 +1,36 @@
-console.log('yt scrpit injected')
 
 chrome.runtime.onMessage.addListener( message => {
   let msg = JSON.stringify(message);
 
   console.log("here in yt script");
   if(message.type === 'yt content script injected') {
-    console.log('yt script injected successfully');
+    //get the youtube video element
+    waitForElm('.ytp-player-content, .ytp-iv-player-content').then((yt_video_elem) => {
+      console.log('Element is ready');
+      console.log(yt_video_elem);
+    });
   }
 
   return true;
 });
+
+//from https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+function waitForElm(selector) {
+  return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+          if (document.querySelector(selector)) {
+              resolve(document.querySelector(selector));
+              observer.disconnect();
+          }
+      });
+
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
+}
